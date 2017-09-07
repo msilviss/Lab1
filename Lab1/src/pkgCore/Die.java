7603ad1cea06230ac543008592f8bcabe456d671
mattheius <mattheius@matts-mbp.fios-router.home>
2017-09-07 06:29:51 -0400

    new

--- Lab1/src/pkgCore/Die.java
+++ Lab1/src/pkgCore/Die.java
@@ -1,14 +1,15 @@
 package pkgCore;
 
 public class Die {
-
-	private int DieValue;
-
-	public Die() {
-		// TODO: Determine DieVaue.. a random number between 1 and 6
-	}
-
-	public int getDieValue() {
-		return DieValue;
-	}
+  private int DieValue;
+  
+  public Die()
+  {
+    DieValue = ((int)(Math.random() * 6.0D + 1.0D));
+  }
+  
+  public int getDieValue() {
+    return DieValue;
+  }
 }
+
--- Lab1/src/pkgTest/DieTest.java
+++ Lab1/src/pkgTest/DieTest.java
@@ -1,21 +1,22 @@
 package pkgTest;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
-
 import pkgCore.Die;
 
 public class DieTest {
 
-	@Test
-	public void TestDie() {
-
-		for (int a = 0; a < 10000; a++) {
-			Die d = new Die();
-			// TODO: Make sure value of die is between 1 and 6.
-		}
-
-	}
-
+  public DieTest() {}
+  
+  @Test
+  public void TestDie()
+  {
+    for (int a = 0; a < 10000; a++) {
+      Die d = new Die();
+      Assert.assertTrue("Die less than or equal to 6", d.getDieValue() <= 6);
+      
+      Assert.assertTrue("Die greater than or equal to 1 ", d.getDieValue() >= 1);
+      System.out.println("val " + d.getDieValue());
+    }
+  }
 }
